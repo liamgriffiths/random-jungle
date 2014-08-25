7e1aabb64c942d288ea6a08c2423ae9684430fa6
Liam Griffiths <liamgriffiths@gmail.com>
2014-08-25 01:05:23 -0400

    Update curly brace spacing

--- src/cart.js
+++ src/cart.js
@@ -17,9 +17,9 @@ const create = (X, Y, labels) => {
   let results = { probs };
 
   if (score > 0) {
-    let {gain, partitions, index, value, fn} = getBestSplit(score, X, Y);
+    let {gain, partitions, i, value, fn} = getBestSplit(score, X, Y);
     let trees = map(partitions, ([x, y]) => create(x, y, labels));
-    assign(results, {gain, trees, index, value, fn});
+    assign(results, { gain, trees, i, value, fn });
   }
 
   return results;
@@ -31,33 +31,27 @@ const predict = (tree, x) =>
 
 // Return the difference between the score (previous entropy) and the sum of the
 // weighted entropy of a set of partitions.
-const informationGain = (score, partitions) => {
-  let parts = values(partitions);
-  let len = flatten(parts).length;
-  return parts.reduce((g, [X, Y]) => g - (Y.length / len) * entropy(Y), score);
-};
+const informationGain = (score, count, ...partitions) =>
+  partitions.reduce((g, [x, y]) => g - (y.length / count) * entropy(y), score);
 
 // Return an array of probabilities for each label.
 const probabilities = (Y, labels) =>
   labels.map(label => perc(label, Y));
 
 // For a feature matrix, return an array of unique feature values for each col.
-// For example: [[1], [1]] -> [{ index: 0, value: 1 }]
 const getFeaturesToSplitOn = (X) =>
-  flatMap(transpose(X), (row, index) => {
-    return uniq(row).map(value => { return {value, index}; });
-  });
+  flatMap(transpose(X), (row, i) => uniq(row).map(value => ({ value, i })));
 
 // Find the best partition by trying all possible partitions and keeping track
 // of the one with the greated 'information gain' (less entropy post-partition).
 const getBestSplit = (score, X, Y) =>
-  getFeaturesToSplitOn(X).reduce((best, {value, index}) => {
-    let fn = ([x, y]) => x[index] >= value;
+  getFeaturesToSplitOn(X).reduce((best, { value, i }) => {
+    let fn = ([x, y]) => x[i] >= value;
     let partitions = map(partition(fn, pack(X, Y)), part => unpack(part));
-    let gain = informationGain(score, partitions);
+    let gain = informationGain(score, Y.length, ...values(partitions));
 
     if (!best.gain || gain > best.gain)
-      assign(best, {gain, partitions, index, value, fn});
+      assign(best, { gain, partitions, i, value, fn });
 
     return best;
   }, {});
